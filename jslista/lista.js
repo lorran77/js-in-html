1eda3a3bca3e0b56efd85acef093053d1e4183d7
lorran azevedo <lorranazevedo153@gmail.com>
2023-07-04 21:56:34 -0300

    adição de funções na lista

--- jslista/lista.js
+++ jslista/lista.js
@@ -5,9 +5,39 @@ function inserir()
     let textnode= document.createTextNode(itemusuario);
 
     let elementofilholi = document.createElement('li');
+    // elementofilholi.style.listStyle = 'number'; 
     elementofilholi.appendChild(textnode);
 
-    let elementopaiul = document.getElementById('lista');
-    elementopaiul.appendChild(elementofilholi);
+    let elementopaiol = document.getElementById('lista');
+    elementopaiol.appendChild(elementofilholi);
 }
+
+
+
+function apagarultimo()
+{
+    let elementopaiol = document.getElementById('lista');
+    let itemremover = elementopaiol.lastElementChild;
+    elementopaiol.removeChild(itemremover);
+}
+
+function apagarprimeiro()
+{
+    let elementopaiol = document.getElementById('lista');
+    let itemremover = elementopaiol.firstElementChild;
+    elementopaiol.removeChild(itemremover);
+}
+
+function apagarespecifico()
+{
+    let elementopaiol = document.getElementById('lista');
+    let itemremover = document.getElementById('campoNumRemover').value;
+
+    let item = elementopaiol.childNodes[itemremover];
+    elementopaiol.removeChild(item);
+}
+
+
+
+    
 
